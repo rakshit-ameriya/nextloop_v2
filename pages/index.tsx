d3fb42c7dfb6932e339a73c301093c47dafca70b
Rakshit Ameriya <rakshitameriya@gmail.com>
2022-05-05 15:25:01 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>NextLoop</title>
+        <meta name="description" content="Nextloop Technologies Pvt. Ltd." />
+        <link rel="icon" href="/logo-white.jpg" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
