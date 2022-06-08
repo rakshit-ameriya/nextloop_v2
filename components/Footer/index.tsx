f7a705760c3ab04b310e3a496d3dfb2025cac7a1
NarendraSisodiya <nsisodiya@nextlooptechnologies.com>
2022-06-08 18:43:33 +0530

    resolved PR on footer section

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import Constants from '../../constants';
 import { TEXT_CONSTANTS } from '../../types/enum';
-import img from 'next/image';
+import Image from 'next/image';
+
+const images = [
+  {
+    id: 1,
+    image: '/Youtube.png',
+  },
+  {
+    id: 2,
+    image: '/Instagram.png',
+  },
+  {
+    id: 3,
+    image: '/Facebook.png',
+  },
+  {
+    id: 4,
+    image: '/Twitter.png',
+  },
+];
 
 function Footer() {
   return (
@@ -25,28 +44,12 @@ function Footer() {
           <span className="text-white font-bold">
             {TEXT_CONSTANTS.FOLLOW_US}:
           </span>
-          <div className=" mt-8 flex justify-start space-x-4 ">
-            <img
-              className="object-contain h-6 w-6   "
-              src="/Youtube.png"
-              alt="Youtube"
-            />
-
-            <img
-              className="object-contain h-6 w-6  "
-              src="/Instagram.png"
-              alt="Instagram"
-            />
-            <img
-              className="object-contain h-6 w-6  "
-              src="/Facebook.png"
-              alt="Facebook"
-            />
-            <img
-              className="object-contain h-6 w-6  "
-              src="/Twitter.png"
-              alt="Twitter"
-            />
+          <div className="mt-8 flex justify-start space-x-4">
+            {images.map(img => (
+              <div key={img.id} className="flex justify-start mx-4 ">
+                <Image width={'30'} height={'30'} src={img.image} alt="Image" />
+              </div>
+            ))}
           </div>
         </div>
 
@@ -55,9 +58,9 @@ function Footer() {
             <p className="text-white font-bold text-2xl pb-4 underline decoration-green-500/[.33] ">
               {TEXT_CONSTANTS.DISCOVER}
             </p>
-            {Constants.DISCOVER.map((items, index) => (
+            {Constants.DISCOVER.map((items, id) => (
               <li
-                key={index}
+                key={id}
                 className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer"
               >
                 {items}
@@ -70,9 +73,9 @@ function Footer() {
             <p className="text-white font-bold text-2xl pb-4 underline decoration-green-500/[.33]">
               {TEXT_CONSTANTS.INFORMATION}
             </p>
-            {Constants.INFORMATION.map((items, index) => (
+            {Constants.INFORMATION.map((items, id) => (
               <li
-                key={index}
+                key={id}
                 className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer"
               >
                 {items}
@@ -85,9 +88,9 @@ function Footer() {
             <p className="text-white font-bold text-2xl pb-4 underline decoration-green-500/[.33] ">
               {TEXT_CONSTANTS.HELP_AND_SUPPORT}
             </p>
-            {Constants.SUPPORT.map((items, index) => (
+            {Constants.SUPPORT.map((items, id) => (
               <li
-                key={index}
+                key={id}
                 className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer"
               >
                 {items}
@@ -100,9 +103,9 @@ function Footer() {
             <p className="text-white font-bold text-2xl pb-4 underline decoration-green-500/[.33] ">
               {TEXT_CONSTANTS.CONTACT}
             </p>
-            {Constants.CONTACT.map((items, index) => (
+            {Constants.CONTACT.map((items, id) => (
               <li
-                key={index}
+                key={id}
                 className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer"
               >
                 {items}
@@ -110,10 +113,12 @@ function Footer() {
             ))}
           </ul>
         </div>
-        <div className="mb-5 flex justify-center w-14 h-14 border-b-0 border-t-0  bg-[#0c0c0cd5] rounded-full">
-          <img
-            className="object-contain h-10 w-10 mt-2 "
+        <div className="mb-5 flex justify-center w-8 h-8 border-b-0 border-t-0  bg-[#0c0c0cd5] rounded-full">
+          <Image
+            className="object-contain h-15 w-15 mt-2 "
             src="/icon.png"
+            width={'30'}
+            height={'30'}
             alt="arrow-up"
           />
         </div>
